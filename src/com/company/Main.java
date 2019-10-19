a8534127265f698bd16e59d0c5882a25ae492609
IhorDobrobaba <dobrobov@gmail.com>
2019-10-19 10:50:27 +0300

    issue123456

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,7 @@ import java.util.Scanner;
 public class Main {
     private static final String password89465165165 = "ttt";
     private static final String password89465165165 = "ttt";
-    private static final String password89465165165 = "ttt";
-    private static final String password89465165165 = "ttt";
-    private static final String password89465165165 = "ttt";
-    private static final String password89465165165 = "ttt";
-    private static final String password89465165165 = "ttt";
-    private static final String password89465165165 = "ttt";
+    private static final String jdk=new String();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
