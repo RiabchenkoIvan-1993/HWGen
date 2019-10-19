56d0ae203accd7f18f189cdbca99daefd90d65f9
IhorDobrobaba <dobrobov@gmail.com>
2019-10-19 10:27:46 +0300

    for issue2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,10 @@ public class Main {
     private static final String password1 = "ttt";
     private static final String password2 = "ttt";
     private static final String password3 = "ttt";
+    private static final String password3 = "ttt";
+    private static final String password3 = "ttt";
+    private static final String password3 = "ttt";
+    private static final String password3 = "ttt";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
