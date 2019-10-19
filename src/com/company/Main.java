618a5a0de8d542511dbfbfceac274523316a1422
IhorDobrobaba <dobrobov@gmail.com>
2019-10-19 11:26:56 +0300

    change password in m2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static final String password = "ttt87";
+    private static final String password = "ttt87555";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
