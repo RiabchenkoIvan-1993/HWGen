16b4f24dcfe4b55aab96e34e50577ca79283108c
IhorDobrobaba <dobrobov@gmail.com>
2019-10-19 10:28:59 +0300

    for issue2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 
 public class Main {
     private static final String password = "ttt";
+    private static final String password = "ttt";
+    private static final String password = "ttt";
+    private static final String password = "ttt";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
